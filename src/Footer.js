141c01fca88fdd0224618660804a8a97832c05cd
sopastore <pastore.sof@gmail.com>
2022-11-22 19:53:09 -0300

    Added footer

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        <a href="https://github.com/sopastore/react-weather-app">
+          Open-source code
+        </a>
+        , by{" "}
+        <a href="https://phenomenal-alpaca-b8ef22.netlify.app/">
+          Sofia Pastore
+        </a>
+      </p>
+    </div>
+  );
+}
+export default Footer;
