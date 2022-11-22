c25af7abb17de51f2adf5ed6c6484dfccae84c4a
sopastore <pastore.sof@gmail.com>
2022-11-22 18:38:27 -0300

    Added search engine and if statement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  let [weather, setWeather] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+  let [city, setCity] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = `aca4dd3643b89e94dbd3cac6cf6f2638`;
+    let units = `metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function showWeather(response) {
+    console.log({ response });
+    setWeather(true);
+    setLoaded(true);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form>
+      <input type="text" placeholder="Enter a city.." onChange={updateCity} />
+      <input type="submit" value="Search" onClick={handleSubmit} />
+    </form>
   );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li> Se llego</li>
+          <li>I can do it 💪🏼</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
 
 export default App;
